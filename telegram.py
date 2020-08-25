b5bc8dc374fd38d6e1c82838791bc9a202152a72
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2020-08-25 11:45:38 +0500

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -27,7 +27,7 @@ def main():
         if update_id == last_update(get_updates_json(url))['update_id']:
            send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
            update_id += 1
-        sleep(2)       
+        sleep(1)       
 
 if __name__ == '__main__':  
     main()
