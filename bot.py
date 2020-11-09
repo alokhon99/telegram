e6d8fee6441402679cba2a86f3d54fbce846603d
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2020-11-09 12:16:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,6 +71,7 @@ class Team:
         elif today == date and self.hour > 12:
             tz = pytz.timezone('Asia/Tashkent')
             now = datetime.datetime.now(tz)
+            print(now)
             my_time_string = self.hour+':'+self.minute+':'+'00'
             my_datetime = datetime.datetime.strptime(my_time_string, "%H:%M:%S")
             my_datetime = now.replace(hour=my_datetime.time().hour, minute=my_datetime.time().minute, second=my_datetime.time().second, microsecond=0)
