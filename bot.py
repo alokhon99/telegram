bb167d392d320945f0c50ef642a785269db9023c
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2020-11-10 12:03:25 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ import pytz
 import datetime
 import os
 import psycopg2
+from dbhelper import DBHelper
 
+db = DBHelper()
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
