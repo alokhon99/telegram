b1b6361b31db5c7fd325597bc531f05248359ec7
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2020-11-16 10:00:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -141,7 +141,7 @@ class Match:
     def get_message(self):
         if self.which == 'p':
             return 'Последный матч: ' + '\n' + self.tournament + self.date+ ' ' + self.hour+':'+self.minute + '\n' +  self.team1 + ' '+self.score+' '+ self.team2
-        return 'Следующий матч: ' + '\n' + self.tournament + self.date+ ' ' + self.hour+':'+self.minute + '\n' +  self.team1 + ' '+ '-'+ self.team2    
+        return 'Следующий матч: ' + '\n' + self.tournament + self.date+ ' ' + self.hour+':'+self.minute + '\n' +  self.team1 + ' - '+ self.team2    
      
         
     team1 = 'team'
