bca0dce5a55a41f77707acd95118c87d41f89e29
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2020-11-18 10:52:56 +0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -119,10 +119,14 @@ class Match:
         
     
     def is_passed(self):
+        print('is_passed1')
         dt = self.date.split(' ')
         day = str(int(dt[0]))
+        print('is_passed2')
         mon = int_value_from_ru_month(dt[1])
+        print('is_passed3')
         date = datetime.datetime.strptime(day+'/'+mon+'/2020', "%d/%m/%Y").date()
+        print('is_passed4')
         today = date.today()
         if today > date:
             return True
