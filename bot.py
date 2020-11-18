8002772457e5f4c462ad6590c25bfd65526e458e
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2020-11-18 17:48:18 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -443,7 +443,8 @@ def obuna(job, x=None, old=' '):
     if x:
         team = teams.get(x.fav)
         print(str(x.chat_id+old)
-        print(job.get_jobs_by_name(str(x.chat_id)+old))
+        a = job.get_jobs_by_name(str(x.chat_id) + old )
+        print(a)
         for j in job.get_jobs_by_name(str(x.chat_id)+old):
                 j.schedule_removal()
                 print(str(x.chat_id)+old)
