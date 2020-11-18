f248419dfdbc050c0b7a8b99b86bad28922323bc
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2020-11-18 14:23:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -429,15 +429,15 @@ def obuna(job):
     print(users)
     for user in users:
         print(user[2])
-        if user[2] == None:
+        if user[2]:
             team = teams.get(user[2])
             match = team.next
             dt = match.date.split(' ')
             day = int(dt[0])
             mon = int_value_from_ru_month(dt[1])
-#             d = datetime.datetime(2020, mon, day, int(match.hour), int(match.minute))
-            d = datetime(2020, 11, 18, 14, 18)
-            print(d)
+            ten_minute = datetime.timedelta(minutes=10)
+            d = datetime.datetime(2020, mon, day, int(match.hour), int(match.minute))
+            print(d-ten_minute)
             tshv = pytz.timezone("Asia/Tashkent")
             d = tshv.localize(d)
             a = job.run_once(callback=callback, when=d,context= (int(user[0]), match.get_message()))
