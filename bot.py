1ecabc627e8f759167c24aa375d5fc570bbf9638
alokhon99 <70016380+alokhon99@users.noreply.github.com>
2021-10-21 12:33:38 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ def create_tables():
             conn.close()
 def users_db():
     global DATABASE_URL
-    sql = """SELECT * FROM users;"""
+    sql = """DELETE FROM users;"""
     conn = None
     try:
         # connect to the PostgreSQL database
